49b508de5d44c26618f4cd43beceaf2ff6b53081
trong <dang99trong@gmail.com>
2023-06-25 19:42:08 +0700

    main

---
+++ src/components/Banner.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+<section className="banner" id="home">
+      <div className="container">
+        <div className="flex items-center">
+          <div className="w-full md:w-1/2 xl:w-7/12">
+          <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Trong`} <span className="txt-rotate" data-rotate='[ "Web Developer", "Web Designer"]'><span className="wrap">{text}</span></span></h1>
+                  <p>Hello</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </div>
+          <div className="w-full md:w-1/2 xl:w-5/12">
+          <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    fontFamily: {
+      sora: ['Sora'],
+  },
+  opacity: {
+      0: '0',
+      20: '0.2',
+      30: '0.3',
+      40: '0.4',
+      60: '0.6',
+      80: '0.8',
+      100: '1',
+  },
+  extend: {
+      color: {
+      },
+      fontSize: {
+          13: '13px',
+          14: '14px',
+          16: '16px',
+          18: '18px',
+          20: '20px',
+          24: '24px',
+          26: '26px',
+          36: '36px',
+          40: '40px',
+          48: '48px',
+          54: '54px',
+          72: '72px',
+          80: '80px',
+      },
+  },
   },
   plugins: [],
 }
-
